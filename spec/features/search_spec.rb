11fa2a766659043080f0f5ff214629803f402c0d
Angel Byun <abyun22@turing.edu>
2023-05-26 13:41:59 -0600

    Create feature testing for the first member of the fire nation

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -8,5 +8,30 @@ RSpec.feature "User can search for Fire Nation members" do
     click_button "Search For Members"
 
     expect(current_path).to eq(search_path)
+
+    expect(page).to have_content("There are 97 members who live in the Fire Nation")
+    expect(page).to have_css('.member', count: 25)
+
+    within(first('.member')) do
+      within('.name') do
+        expect(page).to have_content("Afiko")
+      end
+
+      within('.photo') do
+        expect(page).to have_css("img[src*='https://vignette.wikia.nocookie.net/avatar/images/2/24/Afiko.png/revision/latest?cb=20121121024128']")
+      end
+
+      within('.affiliation') do
+        expect(page).to have_content("Fire Nation")
+      end
+
+      within('.allies') do
+        expect(page).to have_content("Fire Nation")
+      end
+
+      within('.enemies') do
+        expect(page).to have_content("Aang")
+      end
+    end
   end
 end
